947b242beeb609bdc3709769ee00c0571a3cd5b8
Jeremiah Shirk <jshirk@gmail.com>
2012-07-21 15:20:01 -0500

    Better response callback, won't lose chunks in response data

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
 var monitis = require("./api/api.js");
 
 function res_cb(response){
+  var body = '';
+  var append_chunk = function(chunk) {
+    body += chunk;
+  }
+  var print_body = function() {
+    process.stdout.write(body + "\n");
+  }
   console.log("statusCode: ", response.statusCode);
   console.log("headers: ", response.headers);
-  response.on('data', print_chunk)
+  response.on('data', append_chunk);
+  response.on('end', print_body);
 }
 
-function print_chunk(chunk) {
-  process.stdout.write(chunk + "\n");
-}
-
-monitis.get({action: 'getMonitors'},res_cb);
-monitis.post({action: 'deleteMonitor', monitorId: '4607'}, res_cb);
+monitis.get_monitor_info(868, res_cb);
+// monitis.get({action: 'getMonitors'},res_cb);
+// monitis.post({action: 'deleteMonitor', monitorId: '4607'}, res_cb);
