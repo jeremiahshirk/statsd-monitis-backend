63d1431a048af81b9e92e5d5f51d79bee3d2a806
Jeremiah Shirk <jshirk@gmail.com>
2012-07-23 12:51:02 -0500

    Implementation of flush_stats

--- backend.js
+++ backend.js
@@ -1,7 +1,46 @@
 var monitis = require("./api/api.js");
 
+function print_body(body) {
+  console.log(body);
+}
+
+function missing_monitor_cb (name, type, body) {
+  // if recording a metric finds a monitor doesn't exist, add it
+  // note that we lose one datapoint, since we don't retry the post
+  return function(body) {
+    if(body.status == 'no such monitor') {
+      console.log("Adding missing custom monitor: ", name, ":", type);
+      monitis.add_statsd_monitor(name, type, print_body);
+    }
+  }
+}
+
 var flush_stats = function monitis_flush(ts, metrics) {
   console.log("Called flush_stats");
+  console.log(metrics);
+  for (key in metrics.counters) {
+    monitis.add_result_by_name(
+      key, {count: metrics.counters[key]}, missing_monitor_cb(key,'counter'));
+  }
+  for (key in metrics.gauges) {
+    monitis.add_result_by_name(
+      key, {gauge: metrics.counters[key]}, missing_monitor_cb(key,'gauge'));
+  }
+  for (key in metrics.timers) {
+    var sum = 0;
+    var avg = 0;
+    times = metrics.timers[key];
+    for(var i=0;i<times.length; i++) {
+      sum += times[i];
+    }
+    if (times.length > 0) {
+      avg = sum / times.length;
+    }
+    monitis.add_result_by_name(
+      key, {sum: sum, avg: avg}, missing_monitor_cb(key,'timer'));
+    console.log(metrics.timers)
+  }
+  
 }
 
 var backend_status = function monitis_status(writeCb) {
